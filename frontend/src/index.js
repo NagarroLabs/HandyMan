33b4ec580c6b3c08579745c0facfbf3dadce1fd0
Paianu Vlad-Valentin <valyclone@gmail.com>
2020-04-29 13:10:17 +0300

    Updated gitignore, created eslint config file with airbnb style

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,7 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
