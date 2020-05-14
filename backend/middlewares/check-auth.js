069a80405feff87a674de5988eb7f5298b0f4bae
VladTM23 <48724761+VladTM23@users.noreply.github.com>
2020-05-14 11:31:50 +0300

    Back end/start #10 (#33)
    * Created new branch
    
    * Backend architecture start.
    
    * Added custom validation for signup.
    
    * Added password encryption with bcrypt and JWT signup/login.
    
    * Added middleware for checking authentication.
    
    * Fixed error handling issue.
    
    * Added Readme file.
    
    * Updated Readme with env variables
    
    * Added getUserById and updatUser.
    
    * Added getUserById and updateUser.
    
    * Updated Readme.md

---
+++ backend/middlewares/check-auth.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+
+const HttpError = require('../models/http-error');
+
+//  We add this middleware only in front of the routes where authentication is required
+
+module.exports = (req, res, next) => {
+  // This next check is just for the browser to get to the proper HTTP verb
+
+  if (req.method === 'OPTIONS') {
+    return next();
+  }
+
+  /* We are expecting to have in our headers something like:
+     Authorization: "JWT [insertTokenHere]" */
+
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    if (!token) {
+      throw new Error('Authentication failed!');
+    }
+
+    /* We pass the userId in case any other middleware is interested.
+       It will be in the userData property of the req object. */
+
+    const decodedToken = jwt.verify(token, process.env.TOKEN_ENCRYPTION);
+    req.userData = { userId: decodedToken.userId };
+    next();
+
+    /* In case we get to implement admin routes,
+        we could check if the userId is equal to the admin's ID (could use the find/findIndex method on an array of adminId's) in every route needed, or create a similar middleware to this one where we check for userId in it and only put it in front of the admin routes
+    */
+  } catch (err) {
+    return next(new HttpError('Authentification failed', 403));
+  }
+};
---
+++ backend/models/jobs.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+
+const jobSchema = new mongoose.Schema({
+  jobOwner: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  jobName: {
+    type: String,
+    required: true
+  },
+  jobDescription: {
+    type: String,
+    required: true
+  },
+  jobCategory: {
+    type: String,
+    required: true
+  },
+  jobBudget: {
+    type: String,
+    required: true
+  },
+  jobStartDate: {
+    type: Date,
+    required: true
+  },
+  jobCompletionTimeFrame: {
+    type: Date,
+    required: true
+  },
+  jobReqSkills: {
+    type: [String],
+    required: true
+  },
+  jobDifficulty: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true
+  },
+  jobCountry: {
+    type: String,
+    required: true
+  },
+  jobCity: {
+    type: String,
+    required: true
+  },
+  jobAdress: {
+    type: String,
+    required: true
+  },
+  jobComments: {
+    title: String,
+    postBody: String
+  },
+  jobApplicants: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: 'HandyMan'
+    }
+  ],
+  jobAssignee: {
+    type: mongoose.Types.ObjectId,
+    ref: 'HandyMan'
+  }
+});
+
+module.exports = mongoose.model('Job', jobSchema);
