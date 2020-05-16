cbc19f53118317244704859382e5e4b94a8afc5a
Paianu Vlad-Valentin <valyclone@gmail.com>
2020-05-16 11:04:28 +0300

    Added token expiration logic on FE.

--- frontend/src/shared/hooks/auth-hook.js
+++ frontend/src/shared/hooks/auth-hook.js
@@ -1,36 +1,73 @@
 import { useState, useCallback, useEffect } from 'react';
 
+let logoutTimer;
+
 export const useAuth = () => {
   const [token, setToken] = useState();
   const [userId, setUserId] = useState();
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
 
   // Persisting userId and token in the browser's localStorage
 
-  const login = useCallback((uid, _token) => {
+  const login = useCallback((uid, _token, expirationDate) => {
+    // You can pass only the first 2 elements here (when calling it from an Auth-like component)
     setToken(_token);
     setUserId(uid);
-    localStorage.setItem('userData', JSON.stringify({ userId: uid, token: _token }));
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60 * 6); //6 hours (can be modified, but must be in sync with the backend)
+    setTokenExpirationDate(tokenExpirationDate);
+
+    localStorage.setItem(
+      'userData',
+      JSON.stringify({
+        userId: uid,
+        token: _token,
+        expiration: tokenExpirationDate.toISOString()
+      })
+    );
   }, []);
 
   const logout = useCallback(() => {
     setToken(null);
     setUserId(null);
+    setTokenExpirationDate(null);
     localStorage.removeItem('userData');
   }, []);
 
   // Keep the user logged in until his token expires
 
   useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [logout, token, tokenExpirationDate]);
+
+  useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem('userData'));
-    if (storedData && storedData.token) {
-      login(storedData.userId, storedData.token);
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
     }
   }, [login]);
 
   // When using this hook, you can get ahold of any of these:
 
   return {
-    userId, token, login, logout
+    userId,
+    token,
+    login,
+    logout
   };
 
   /* These will be available in any component (will allow you to check
@@ -48,6 +85,4 @@ export const useAuth = () => {
      -> auth.userId === wantedId
      -> auth.login(id, token)
      */
-
-
 };
