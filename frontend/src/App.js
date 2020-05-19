23be61403915d4ba0267fe8e098626715eb26405
Alex Rotariu <49029073+alex-rotariu@users.noreply.github.com>
2020-05-19 17:40:35 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,43 +2,10 @@ import React from "react";
 import * as usersApi from "./mock-api/usersApi";
 
 function App() {
-  // const user = {
-  //   id: "",
-  //   HandyMan: 0,
-  //   firstName: "Jack",
-  //   lastName: "Chan",
-  //   email: "jackiechan@gmail.com",
-  //   phone: "323243243",
-  //   gender: "Male",
-  //   birthDate: "1964-03-07T07:48:19Z",
-  //   userName: "JackC",
-  //   password: "jackie",
-  //   employedHandyMen: [],
-  // };
-
-  // return (
-  //   <div>
-  //     <h1>HandyMan App</h1>
-  //     <button
-  //       onClick={() => {
-  //         usersApi.deleteUser(6);
-  //         usersApi.getUsers().then((users) => console.log(users));
-  //       }}
-  //     >
-  //       Delete user
-  //     </button>
-  //     <button
-  //       onClick={() => {
-  //         usersApi.addUser(user);
-  //         usersApi.getUsers().then((users) => console.log(users));
-  //       }}
-  //     >
-  //       Add user
-  //     </button>
-  //   </div>
-  // );
-  return;
-  <h1>HandyMan App</h1>;
+  return
+  <h1>
+    HandyMan App
+  </h1>;
 }
 
 export default App;
