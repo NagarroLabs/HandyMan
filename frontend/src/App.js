32c79400a308068b6bedcc7b0c3f2fa371b965af
Alex Rotariu <49029073+alex-rotariu@users.noreply.github.com>
2020-05-19 19:31:00 +0300

    Back end/mock api database (#34)
    * Created Mock API database
    
    - json-server api
    - deleted boilerplate files from frontend
    - users, handyMen, jobs and reviews mock entries
    - get and getById methods for each endpoint
    
    * Added POST, PUT and DELETE to mock api
    
    * Update App.js
    
    * Update handyMenApi.js
    
    * Update jobsApi.js
    
    * Update reviewsApi.js
    
    * Update usersApi.js
    
    * Update apiServer.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as usersApi from "./mock-api/usersApi";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return
+  <h1>
+    HandyMan App
+  </h1>;
 }
 
 export default App;
