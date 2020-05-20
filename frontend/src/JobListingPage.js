2298c450404329a3ab1059a45c49ea8787353758
Alex Rotariu <49029073+alex-rotariu@users.noreply.github.com>
2020-05-21 01:15:55 +0300

    Basic filtering

--- frontend/src/JobListingPage.js
+++ frontend/src/JobListingPage.js
@@ -6,30 +6,73 @@ import JobsList from "./components/JobsList";
 import FilterBox from "./components/FilterBox";
 
 class JobListingPage extends React.Component {
+  state = {
+    jobs: [],
+    filteredJobs: [],
+    showJobs: [],
+    searchField: "",
+  };
   constructor() {
     super();
-    this.state = {
-      jobs: [],
-      searchField: "",
-    };
+    this.filterJobs = this.filterJobs.bind(this);
   }
 
   componentDidMount() {
     jobsApi.getJobs().then((jobsList) => {
-      this.setState({ jobs: jobsList });
+      this.setState({
+        jobs: jobsList,
+        filteredJobs: jobsList,
+        showJobs: jobsList,
+      });
     });
+  }
+
+  filterJobs(priceTo, specialization, sortOrder) {
+    let result = this.state.jobs;
+    if (priceTo) {
+      result = result.filter((job) => {
+        return job.estimatedBudget < priceTo;
+      });
+    }
+    if (priceTo === 0) {
+      this.setState({
+        showJobs: this.state.jobs,
+        filteredJobs: this.state.jobs,
+      });
+      return;
+    }
+    if (sortOrder) {
+      if (sortOrder === "ascendingprice") {
+        result = result.sort((a, b) => a.estimatedBudget - b.estimatedBudget);
+      } else {
+        result = result.sort((a, b) => b.estimatedBudget - a.estimatedBudget);
+      }
+    }
+    if (result.length !== this.state.jobs.length) {
+      this.setState({
+        filteredJobs: result,
+        showJobs: result,
+        searchField: "",
+      });
+    } else {
+      result = this.state.jobs;
+      this.setState({
+        showJobs: result,
+      });
+    }
   }
 
   onSearchChange = (event) => {
     this.setState({ searchField: event.target.value });
+
+    var result = this.state.filteredJobs.filter((job) => {
+      return job.name.toLowerCase().includes(event.target.value.toLowerCase());
+    });
+    console.log(result);
+    this.setState({ showJobs: result });
   };
 
   render() {
-    const filteredJobs = this.state.jobs.filter((job) => {
-      return job.name
-        .toLowerCase()
-        .includes(this.state.searchField.toLowerCase());
-    });
     if (this.state.jobs.length === 0) {
       return <h1>Loading...</h1>;
     } else {
@@ -42,11 +85,11 @@ class JobListingPage extends React.Component {
           <div className="mw9 center ph3-ns pa5">
             <div className="cf ph2">
               <div className="fl w-100 w-50-m w-25-l">
-                <FilterBox />
+                <FilterBox filter={this.filterJobs} />
               </div>
               <div className="fl w-100 w-50-m w-75-l">
                 <ErrorBoundry>
-                  <JobsList jobs={filteredJobs} />
+                  <JobsList jobs={this.state.showJobs} />
                 </ErrorBoundry>
               </div>
             </div>
--- frontend/src/components/FilterBox.js
+++ frontend/src/components/FilterBox.js
@@ -1,22 +1,117 @@
-import React from "react";
+import * as React from "react";
 
-const FilterBox = ({ searchfield, searchChange }) => {
-  return (
-    <div className="pa1">
-      <form className="pa3 ba b--black-10">
-        <fieldset className="cf bn ma0 pa0">
-          <legend className="pa0 f5 mb3 black-80">Filters</legend>
-          <div className="cf">
-            <input
-              className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 br2-ns br--right-ns"
-              type="submit"
-              value="Filter"
-            ></input>
+class FilterBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      priceTo: "",
+      specialization: "",
+      sortOrder: "",
+      sortOrders: ["Ascending price", "Descending price"],
+    };
+  }
+
+  submitForm = (e) => {
+    e.preventDefault();
+    this.props.filter(
+      this.state.priceTo,
+      this.state.specialization,
+      this.state.sortOrder
+    );
+  };
+
+  render() {
+    const postcodes = [];
+    return (
+      <div className="">
+        <form className="" onSubmit={this.submitForm}>
+          <p className="mb-1">Refine your results</p>
+          <div className="columns text-center">
+            <div className="column col-4 col-xs-12">
+              <div className="form-group">
+                <div className="col-3 col-sm-12">
+                  <label className="form-label" htmlFor="price-from">
+                    Price up to
+                  </label>
+                </div>
+                <div className="col-9 col-sm-12">
+                  <input
+                    className="form-input"
+                    min="0"
+                    max="10000000"
+                    type="number"
+                    id="price-from"
+                    placeholder="£1,000,000"
+                    value={this.state.priceTo}
+                    onChange={(event) =>
+                      this.setState({ priceTo: Number(event.target.value) })
+                    }
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="column col-4 col-xs-12">
+              <div className="form-group">
+                <div className="col-3 col-sm-12">
+                  <label className="form-label" htmlFor="specialization">
+                    Specialization
+                  </label>
+                </div>
+                <div className="col-9 col-sm-12">
+                  <select
+                    className="form-select"
+                    id="specialization"
+                    value={this.state.specialization}
+                    onChange={(event) =>
+                      this.setState({ specialization: event.target.value })
+                    }
+                  >
+                    <option value="">Choose...</option>
+                    {postcodes.map((pc) => (
+                      <option key={pc} value={pc.toLowerCase()}>
+                        {pc}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="column col-4 col-xs-12">
+              <div className="form-group">
+                <div className="col-3 col-sm-12">
+                  <label className="form-label" htmlFor="sortorder">
+                    Sort Order
+                  </label>
+                </div>
+                <div className="col-9 col-sm-12">
+                  <select
+                    className="form-select"
+                    id="sortorder"
+                    value={this.state.sortOrder}
+                    onChange={(event) =>
+                      this.setState({ sortOrder: event.target.value })
+                    }
+                  >
+                    <option value="">Choose...</option>
+                    {this.state.sortOrders.map((order) => (
+                      <option
+                        key={order}
+                        value={order.replace(" ", "").toLowerCase()}
+                      >
+                        {order}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+            </div>
           </div>
-        </fieldset>
-      </form>
-    </div>
-  );
-};
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default FilterBox;
